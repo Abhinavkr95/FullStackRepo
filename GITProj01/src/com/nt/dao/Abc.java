48e17e8c0850c5dbabf7fd336597ed8058ca1e20
abhinav_kumar <abhinavku1913@gmail.com>
2024-07-17 01:46:33 +0530

    Tl Commit

--- GITProj01/src/com/nt/dao/Abc.java
+++ GITProj01/src/com/nt/dao/Abc.java
@@ -4,7 +4,7 @@ public class Abc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello Abhinav I am TL : ");
+		System.out.println("hello Abhinav I am TL   h: ");
 		Xyz xyz = new Xyz();
 		xyz.m1();
 
