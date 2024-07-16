b90ba319f04c351729b1745c4f4b594929bba533
Abhishek <Abhishek@DESKTOP-5VJGJ96>
2024-07-16 17:11:53 +0530

    Add welcome

--- GITProj01/src/com/nt/dev/HelloTeam.java
+++ GITProj01/src/com/nt/dev/HelloTeam.java
@@ -4,7 +4,7 @@ public class HelloTeam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello team how are you bros ");
+		System.out.println("Hello team how are y u bros ");
 	}
 
 }
