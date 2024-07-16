002e49a9f951df223e6399807bc668e7acc562ef
Abhishek <Abhishek@DESKTOP-5VJGJ96>
2024-07-16 17:03:20 +0530

    Hello team this msg is from dev side

---
+++ GITProj01/src/com/nt/dev/HelloTeam.java
@@ -0,0 +1,10 @@
+package com.nt.dev;
+
+public class HelloTeam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello team how are you ");
+	}
+
+}
