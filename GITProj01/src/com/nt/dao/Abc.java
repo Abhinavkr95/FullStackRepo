615e74eb5588575aaf46c046d43dffe4e114b745
abhinav_kumar <abhinavku1913@gmail.com>
2024-07-24 23:39:46 +0530

    done by tl

--- GITProj01/src/com/nt/dao/Abc.java
+++ GITProj01/src/com/nt/dao/Abc.java
@@ -5,11 +5,11 @@ public class Abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello Abhinav I am TL :  yes");
 		System.out.println("hello Abhinav I am TL :  no");
 
+
 		Xyz xyz = new Xyz();
-		xyz.m1();
+		xyz.m2();
 
 	}
 
